80894b9897529c92e560783068a71acf6b83012b
Rachel Ober <rachelober@gmail.com>
2010-03-10 20:58:36 -0500

    Adding Kamaal'a the Raveen Kat

---
+++ gryphonice/raveenkats/kemaala.php
@@ -0,0 +1,86 @@
+<?php 
+$animal = "Raveen Kats";
+$name = "Kamaal'a";
+include('../../files/header.php'); ?>
+<div id="CONTENT"> 
+  <p align="center"><img src="Fate_Female_BrightPromise.png" width="718" height="580" alt="Kamaal'a"></p>
+	<h2>Spirit-Specific Powers</h2>
+	<p>Nature Immunity (WoRs powers/mutation cannot effect/damage nature), X'ysahtu WoR Immunity (immune to powers/mutations of others from X'ysahtu), Earth-Spirit Sight, Perfect Nightvision, Aura Sight, Immunity to earth-based powers, Immunity to mind control/thralls/manipulation, immunity to instant-kills. Takes extra damage from Soul Damaging powers/weapons. Plant-like growth on shoulder is immune to harm.</p>
+	<h2>Powers</h2>
+	<p>Soul-reading, Teleportation, Fate Sight (can see the destiny/fate of others by meeting their gaze), Change of Fate (can alter destinies of others), Protective Sigils.</p>
+  <h2>Notes</h2>
+  <p>&quot;Taken in by the Spirits of X'ysahtu, she was bonded to an earthen spirit whose influences show in the small glowing tree-like growth on her shoulders. Her mutation was once haywire, harmful to not only herself but others as well, but once the spirit bonded with her the mutation became stable. Her Elemental Manifestation shows in the form of gem-crystal growths jutting from the back of her shoulders, her face being more gem than flesh, her forelimbs being made of cold energies, and wings/tail being a mix of silver and blue energies. Her tail ends in a sweeping circle that can actually cause the fates of others to be changed; and what they end up being depends on her emotions at the time. All she has to do is touch them with her tail, and she can save them from a deadly fate, or cause them to have a fatal fate in the future. If she's particularly upset or angry, she can cause them instant death by touching them; stealing their destinies completely. When she's in a pleasant mood she can just alter their fates mildly, allowing for small things of happiness in their future, or riding them of curses or other things that have already marked them. (Her power cannot affect those of X'ysahtu, and if she meets the eyes of one from X'ysahtu, she can't see their futures).&quot;</p>
+	<p>&quot;But this mutation comes with a price. For any Fate she alters, she changes someone else's fate. So by giving a pleasant future to someone, she's cursing someone else with a horrible one. She cannot control who ends up with the exchanged fates, and they're not always individuals she's met. But she can also cause a protective sigil to appear on those she blesses the fates of, making it so another could never alter their fate.&quot;</p>
+  <table>
+    <tr> 
+      <th colspan="2"><?php echo($name); ?></th>
+    </tr>
+    <tr> 
+      <td>Gender:</td>
+      <td>Female</td>
+    </tr>
+		<tr>
+			<td>Parentage:</td>
+			<td><a href="http://www.edgeofshadows.com/ShadowEdge/Raveens/Anki.html">Anki</a> x Tuurite</td>
+		</tr>
+    <tr> 
+      <td>Age:</td>
+      <td>Adult</td>
+    </tr>
+    <tr> 
+      <td>Element:</td>
+      <td>Fate</td>
+    </tr>
+		<tr>
+			<td>World:</td>
+			<td>X'ysahtu</td>
+		</tr>
+		<tr> 
+      <td>Mate:</td>
+      <td>None</td>
+    </tr>
+    <tr> 
+      <td>Offspring:</td>
+      <td>None</td>
+    </tr>
+		<tr> 
+      <td>Birth Pride:</td>
+      <td><a href="http://www.edgeofshadows.com/ShadowEdge/Raveens/Prides/NorthWind.html">Pride 
+        of the North Wind</a></td>
+    </tr>
+    <tr> 
+      <td>Pride:</td>
+      <td>None</td>
+    </tr>
+    <tr> 
+      <td>Dimension:</td>
+      <td>Inner</td>
+    </tr>
+    <tr> 
+      <td>Deity:</td>
+      <td>Orcle</td>
+    </tr>
+    <tr> 
+      <td colspan="2">Raveen Kats &copy; <a href="http://www.battleprides.net/">GryphonIce</a></td>
+    </tr>
+  </table>
+
+	<table>
+		<tr>
+			<th colspan="2">IriKan Info</th>
+		</tr>
+		<tr>
+			<td>Type:</td>
+			<td>Pure</td>
+		</tr>
+		<tr>
+			<td>Personification:</td>
+			<td>Locked</td>
+		</tr>
+		<tr>
+			<td>Power:</td>
+			<td>Locked</td>
+		</tr>
+	</table>
+</div>
+<?php include('../../files/footer.php'); ?>
